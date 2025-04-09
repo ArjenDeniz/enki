ab85533902fe2543a9c7ae008b05cf88738c051b
Arjen Deniz <arjendenizturkmen@gmail.com>
2025-04-09 13:55:01 +0300

    add font helvetica neue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: "https://fonts.cdnfonts.com/css/helvetica-neue-55"
+      }
+    ]
+  },
   // ssr: true,
   // nitro: {
   //   preset: 'static'
