0c85a9cf3aec5b834c55d41fdf39604dc35ed16f
Arjen Deniz <arjendenizturkmen@gmail.com>
2025-04-10 18:06:44 +0300

    hero section draft

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,5 @@
 <template>
-  <div class="home-page">
-      <section class="hero limited">
-        <h2>My Awesome Static Website</h2>
-        <p>This is the homepage of my new Nuxt.js static website.</p>
-      </section>
+      <Hero />
       <Canvas/>
       <Projects/>
-  </div>
 </template>
-
-<style lang="scss" scoped>
-.home-page {
-  width: 100%;
-}
-
-.hero {
-  text-align: center;
-  margin-bottom: 40px;
-}
-
-
-</style>
